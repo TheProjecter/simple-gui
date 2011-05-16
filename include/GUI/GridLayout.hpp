ca7613046c5823dfab5e943b90979c27c37fb715
adrian.tut <adrian.tut@gmail.com>
2011-05-16 19:54:27 +0000

    Hopefully finally fixed the dragging crash. This time it was the hover target cause crashes. GridLayout widget is finally "functional", yet pretty incomplete.

--- include/GUI/GridLayout.hpp
+++ include/GUI/GridLayout.hpp
@@ -11,7 +11,7 @@ namespace gui
 
 		//     upper part
 		//  ______\/______
-		//l |_|________|_| } 5px panning
+		//l |_|________|_| }panning
 		//e | |        | | 
 		//f=|>| proper |<|== right part
 		//t |_|________|_|
@@ -27,21 +27,40 @@ namespace gui
 			RightCollision		//collided with the right part of the rect
 		};
 		LayoutItem(Widget* widget = NULL, bool expanded = false);
-		CollisionType IsCollision(int x, int y) const;
-		void SetSize(const Rect& rect);
-		const Rect& GetSize() const;
+		CollisionType IsCollision(int x, int y, uint32 panning) const;
+
+		void SetPos(int32 x, int32 y);
+		void SetSize(uint32 width, uint32 height, bool extremity);
+
+		void SetRect(int32 x, int32 y, uint32 w, uint32 h);
+		const Rect& GetRect() const;
 
 		void SetWidget(Widget* widget);
 		Widget* GetWidget() const;
 
+		void Draw(sf::RenderWindow& window) const;
+
 		bool empty() const;
 
+		bool IsExpand() const;
+		void SetExpand(bool flag);
+
+		uint32 GetRowSpan() const;
+		uint32 GetColSpan() const;
+
+		void SetRowSpan(uint32 count);
+		void SetColSpan(uint32 count);
+
 		void ResizeWidget(uint32 panning);
+		void UpdateWidgetPos(uint32 panning);
 	private:
 		Rect m_rect;
 		sf::Shape m_shape;
 		Widget* m_widget;
 		bool m_expanded;
+
+		uint32 m_rowspan;
+		uint32 m_colspan;
 	};
 
 	class GridLayout : public Widget
@@ -50,7 +69,7 @@ namespace gui
 		GridLayout();
 		~GridLayout();
 
-		void AddWidgetToGrid(Widget* widget, uint32 line, uint32 column);
+		bool AddWidgetToGrid(Widget* widget, uint32 line, uint32 column);
 		Widget* RemoveWidgetAt(uint32 line, uint32 column);
 
 		void AddLineBefore(uint32 line);
@@ -59,15 +78,25 @@ namespace gui
 		void AddColumnBefore(uint32 column);
 		void AddColumnAfter(uint32 column);
 
-		void HandleDragDrop(Drag* drag);
+		bool HandleDragDrop(Drag* drag);
 		
 		void SetPanning(uint32 panning);
 		uint32 GetPanning() const;
 	private:
 		void ComputeCells();					//internally used to resize the cells
-		void AddWidget(Widget* child);			//only internally used
+		bool AddWidget(Widget* child);			//only internally used
 		bool RemoveWidget(Widget* widget);		//internally used as well
 
+		void RemoveWidgetFromGrid(Widget* widget);
+		void Draw() const;
+
+		void SetPos(int x, int y, bool forceMove /* = false */, bool save /* = true */);
+		void SetPosForGrid();
+
+		void ReloadSettings();
+		
+		void SaveGridProperties();
+		void LoadGridProperties();
 		//lines		//columns
 		std::vector<std::vector<LayoutItem> > m_items;
 		uint32 m_panning;						//panning for widgets
